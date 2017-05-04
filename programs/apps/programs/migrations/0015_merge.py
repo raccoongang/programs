8b9c40b5cdbe28bdddcfe5a9ebd0653f0ea4574f
Raccoon Devops <devops@raccoongang.com>
2017-05-04 09:02:04 +0000

    Fix migrations

---
+++ programs/apps/programs/migrations/0015_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('programs', '0013_auto_20160725_2147'),
+        ('programs', '0014_program_featured'),
+    ]
+
+    operations = [
+    ]
